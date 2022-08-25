4694c27320ee778ee4f5c198343a50bfc9f80d75
Kamrad33 <comradebender33ru@gmail.com>
2022-08-25 21:51:38 +0300

    Fixed custom input component

--- src/components/UI/input/AppInput.jsx
+++ src/components/UI/input/AppInput.jsx
@@ -1,18 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import classes from './AppInput.module.css';
-const AppInput = ({value, children}) => {
-  const [textAreaValue, setTextAreaValue] = useState();
-  console.log('child', value);
-  useEffect(() => {
-    console.log('eff');
-    setTextAreaValue(value);
-  }, [children])
+
+const AppInput = (props) => {
 
   return (
-      <textarea
-      className = {classes.myInput}
-      style = {{maxHeight: '30vh', resize:'vertical', width: '100%'}}
-      defaultValue = {value}/>
+      <textarea className = {classes.myInput} {...props}/>
   );
 };
 
